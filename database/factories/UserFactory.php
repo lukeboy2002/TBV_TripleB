07af7f420283302698acee0f857d02151b9018ae
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2024-10-18 21:08:04 +0200

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,15 +27,15 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'username'                  => fake()->username(),
-            'email'                     => fake()->unique()->safeEmail(),
-            'email_verified_at'         => now(),
-            'password'                  => static::$password ??= Hash::make('password'),
-            'two_factor_secret'         => null,
+            'username' => fake()->username(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
-            'remember_token'            => Str::random(10),
-            'profile_photo_path'        => null,
-            'current_team_id'           => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null,
         ];
     }
 
@@ -44,7 +44,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn(array $attributes) => [
+        return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
@@ -54,15 +54,15 @@ class UserFactory extends Factory
      */
     public function withPersonalTeam(?callable $callback = null): static
     {
-        if (!Features::hasTeamFeatures()) {
+        if (! Features::hasTeamFeatures()) {
             return $this->state([]);
         }
 
         return $this->has(
             Team::factory()
-                ->state(fn(array $attributes, User $user) => [
-                    'name'          => $user->name.'\'s Team',
-                    'user_id'       => $user->id,
+                ->state(fn (array $attributes, User $user) => [
+                    'name' => $user->name.'\'s Team',
+                    'user_id' => $user->id,
                     'personal_team' => true,
                 ])
                 ->when(is_callable($callback), $callback),
