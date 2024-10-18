69ccb36d086a0c80035562ce7e46b3aeb9669d02
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2024-10-18 18:54:24 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     @stack('styles')
     @livewireStyles
 </head>
-<body class="font-sans antialiased bg-white dark:bg-green-600">
+<body class="font-sans antialiased bg-gray-200 dark:bg-menu/80">
 <x-banner/>
 <div class="grid min-h-[100dvh] grid-rows-[auto_1fr_auto]">
     <livewire:navigation-menu/>
